597f540eddabc2faddadd03aa54d0c19e2664322
Mmy26 <yu.b1994masamura@gmail.com>
2022-02-09 17:24:25 +0900

    従業員一覧表示 vueファイルscriptタグ実装

---
+++ src/views/EmployeeList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Employee } from "@/types/employee";
+@Component
+export default class EmployeeList extends Vue {
+  /**
+   * Vuexストアのアクション経由で非同期でWebAPIから従業員一覧を取得する.
+   */
+  created(): void {
+    this.$store.dispatch("getEmployeeList", {});
+  }
+  /**
+   * 非同期で取得したVuexストア内の従業員数を取得し返す
+   */
+  get employeeCount(): number {
+    return this.$store.getters.getEmployeeCount;
+  }
+  /**
+   * 非同期で取得したVuexストア内の従業員一覧を取得し返す
+   */
+  get employees(): Array<Employee> {
+    return this.$store.getters.getEmployees;
+  }
+}
+</script>
+
+<style scoped></style>
