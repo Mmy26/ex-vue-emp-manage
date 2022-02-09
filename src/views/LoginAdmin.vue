e73dd69d5fc20ee0cc3e883b4e262deb59d66ca1
Mmy26 <yu.b1994masamura@gmail.com>
2022-02-09 15:42:07 +0900

    ログイン機能 vueファイルのHTMLの実装

--- src/views/LoginAdmin.vue
+++ src/views/LoginAdmin.vue
@@ -1,5 +1,60 @@
 <template>
-  <div></div>
+  <div>
+    <div class="top-wrapper">
+      <div class="container">
+        <div class="row login-page">
+          <div class="col s12 z-depth-6 card-panel">
+            <div class="error">{{ errorMessage }}</div>
+            <form class="login-form">
+              <div class="row"></div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <i class="material-icons prefix">mail_outline</i>
+                  <input
+                    v-model="mailAddress"
+                    class="validate"
+                    id="mailAddress"
+                    type="email"
+                  />
+                  <label
+                    for="mailAddress"
+                    data-error="wrong"
+                    data-success="right"
+                    >メールアドレス</label
+                  >
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <i class="material-icons prefix">lock_outline</i>
+                  <input v-model="password" id="password" type="password" />
+                  <label for="password">パスワード</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <button
+                    v-on:click="loginAdmin"
+                    type="button"
+                    class="btn btn-register waves-effect waves-light col s12"
+                  >
+                    ログイン
+                  </button>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s6 m6 l6">
+                  <p class="margin medium-small">
+                    <a href="registerAdmin.html">管理者登録はこちら</a>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
